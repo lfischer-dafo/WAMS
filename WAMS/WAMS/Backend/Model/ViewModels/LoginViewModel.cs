60cfbf3021ca8f7621c77cc9e318111a2dddc1dc
LPCrazyCreeper <sebastian.knierim@gmail.com>
2024-09-30 09:34:59 +0200

    C Stundenplan Design hinzugefügt und change SQL Connection

---
+++ WAMS/WAMS/Backend/Model/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WAMS.Backend.Model.ViewModels
+{
+   public class LoginViewModel
+   {
+      [Required(AllowEmptyStrings = false, ErrorMessage = "No Uid")]
+      public string? UserName { get; set; }
+
+      [Required(AllowEmptyStrings = false, ErrorMessage = "No Pw")]
+      public string? Password { get; set; }
+   }
+}
